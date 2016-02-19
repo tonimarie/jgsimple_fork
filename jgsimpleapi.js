ddc6805aae85dcf0f35d109d085073bd5b30247e
anibalsanchez <anibal.sanchez@extly.com>
2016-02-19 19:33:59 +0100

    Copyright update

--- jgsimpleapi.js
+++ jgsimpleapi.js
@@ -1,13 +1,14 @@
 /*!
- * @package: JoomGap Library
+ * @package:  JoomGap Library
  * 
- * @author Name: Andrea Gentil & Anibal Sanchez, JoomGap.com Email:
- * team[at]joomgap.com Url: http://www.JoomGap.com
+ * @author    Name: Andrea Gentil & Anibal Sanchez, JoomGap.com 
+ *            Email: team[at]joomgap.com 
+ *            Url: http://www.JoomGap.com
  * 
- * @copyright Copyright (C) 2015 Andrea Gentil & Anibal Sanchez. All rights
- * reserved. 
+ * @copyright Copyright (C) 2015 - 2016 Andrea Gentil & Anibal Sanchez. 
+ *            All rights reserved. 
  * 
- * @license http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
